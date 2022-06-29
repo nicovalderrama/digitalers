eb6af2f95de0b4a062415ad8eca0bcef85e1c656
NicolasAValderrama <nicovalderrama2002@gmail.com>
2022-06-29 18:19:02 -0300

    .

--- modulo-11/ejercicio1.js
+++ modulo-11/ejercicio1.js
@@ -1,12 +1,15 @@
-let costosFijos=0;
-let costosVariables=0;
-let precio=0;
-//ingresar el valor de los costos fijos
+let costosFijos=2500;
+let costosVariables=400;
+let precio=600;
+
 costosFijos=parseInt(prompt("Ingrese el valor de los costos fijos"));
-//ingresar el valor de los costos variables
+
 costosVariables=parseInt(prompt("Ingrese el valor de los costos variables"));
-//ingresar el valor del precio
+
 precio=parseInt(prompt("Ingrese el valor del precio"));
+
 let cv= costosFijos/(precio-costosVariables);
-alert("El punto de equilibrio es: "+cv);
+
+console.log("El punto de equilibrio: "+cv);
+
 
